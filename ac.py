01e48ba9c281c4cc100f8cb075216e5dfe3ea0e4
aarav-2213 <naniangels2213@gmail.com>
2025-04-10 18:17:28 +1000

    Add files via upload

---
+++ ac.py
@@ -0,0 +1,14 @@
+a=6556
+b=65465
+c=85
+d=42345478
+e=1
+
+
+print("sum:" , a+b+c+d+e)
+
+
+avg=(a+b+c+d+e)/5
+print("average=",(avg))
+
+
---
+++ ac2.py
@@ -0,0 +1,8 @@
+a=int(input("enter your amount"))
+
+
+n1= a//100
+print("100 dollar notes are:",n1)
+
+n2= (a % 100)  //50
+print("50 dollar notes are:", n2)
